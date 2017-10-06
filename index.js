6ab307e165cd954a3e8c98f8e7a70fdf940603c3
MIKUScallion <MIKUScallion@icloud.com>
2017-10-06 18:05:27 +0800

    Fix require preloadjs bug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require('./lib/easeljs/easeljs-0.8.2.combined.js')
 require('./lib/tweenjs/tweenjs-0.6.2.combined.js')
 require('./lib/soundjs/soundjs-0.6.2.combined.js')
-require('./lib/preloadjs/preloadjs-0.6.2.min.js')
+require('./lib/preloadjs/preloadjs-0.6.2.combined.js')
