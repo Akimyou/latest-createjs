e7dac6f8766efc2d3c9b487737d8fa812a648c98
MIKUScallion <MIKUScallion@icloud.com>
2017-10-06 17:04:21 +0800

    Fix this.createjs bug

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,8 @@
-rm -r ./lib
-mkdir ./lib
-cp -r ./submodules/EaselJS/lib ./lib/easeljs/
-cp -r ./submodules/TweenJS/lib ./lib/tweenjs/
-cp -r ./submodules/SoundJS/lib ./lib/soundjs/
-cp -r ./submodules/PreloadJS/lib ./lib/preloadjs/
+rm -r lib
+mkdir lib
+cp -r submodules/EaselJS/lib lib/easeljs/
+cp -r submodules/TweenJS/lib lib/tweenjs/
+cp -r submodules/SoundJS/lib lib/soundjs/
+cp -r submodules/PreloadJS/lib lib/preloadjs/
+
+sed -i '' 's/this.createjs/window.createjs/g' `grep this.createjs -rl lib`
